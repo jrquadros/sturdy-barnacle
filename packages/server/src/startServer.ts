cf0b282993d3c6b239a8ccdd8e35c335792faebd
Júnior Quadros <jrmquadros@gmail.com>
2020-07-22 03:38:49 -0300

    chore(schemas): generate modularized schemas

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -1,29 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga'
-import { importSchema } from 'graphql-import'
-import { join } from 'path'
 import { createTypeOrmConnection } from './utils/createTypeormConnection'
 import { config } from './config'
-
-import * as fs from 'fs'
-import { GraphQLSchema } from 'graphql'
-import { mergeSchemas, makeExecutableSchema } from 'graphql-tools'
-
+import { genSchema } from './utils/genSchemas'
 export const startServer = async () => {
-  const folders = fs.readdirSync(join(__dirname, './modules'))
-
-  const schemas: GraphQLSchema[] = []
-
-  folders.map((folder) => {
-    const { resolvers } = require(join(__dirname, `./modules/${folder}/resolvers`))
-    const typedefs = importSchema(join(__dirname, `./modules/${folder}/schema.graphql`))
-    schemas.push(makeExecutableSchema({ typeDefs: typedefs, resolvers }))
-  })
-
   await createTypeOrmConnection()
 
-  const schema: any = mergeSchemas({ schemas })
-
-  const server = new GraphQLServer({ schema })
+  const server = new GraphQLServer({ schema: genSchema() as any })
 
   const app = await server.start({ port: config.serverPort }).then(() => {
     console.log(`Server running at port ${config.serverPort}`)
