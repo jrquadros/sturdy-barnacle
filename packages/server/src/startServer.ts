045bb0f5e933ba6cef2b37be046d9dd265ea099e
Júnior Quadros <jrmquadros@gmail.com>
2020-07-21 21:22:03 -0300

    chore(server): start server

---
+++ packages/server/src/startServer.ts
@@ -0,0 +1,24 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { addResolversToSchema } from '@graphql-tools/schema'
+import { join } from 'path'
+import { resolvers } from './resolvers'
+import { createTypeOrmConnection } from './utils/createTypeormConnection'
+import { config } from './config'
+
+export const startServer = async () => {
+  await createTypeOrmConnection()
+  const typedefs = loadSchemaSync(join(__dirname, './schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+  })
+
+  const schemaWithResolvers = addResolversToSchema({ schema: typedefs, resolvers })
+  const server = new GraphQLServer({ schema: schemaWithResolvers, resolvers })
+
+  const app = await server.start({ port: config.serverPort }).then(() => {
+    console.log(`Server running at port ${config.serverPort}`)
+  })
+
+  return app
+}
