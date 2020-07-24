ea6bb8a5f6e8614932aee5219a4fe2b1a1170bfc
Júnior Quadros <jrmquadros@gmail.com>
2020-07-24 05:22:47 -0300

    feat(web): router setup

---
+++ packages/web/src/Routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Todos } from './pages/Todos'
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={'/todos'} component={Todos} />
+        <Route render={() => Home()} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
